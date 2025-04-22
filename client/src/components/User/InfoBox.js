dddef11901d2ebb20b477df9b0b2e259aff9f2ac
priyank1 <rautelapriyank5@gmail.com>
2025-04-22 08:54:49 +0530

    update in profile section

--- client/src/components/User/InfoBox.js
+++ client/src/components/User/InfoBox.js
@@ -1,20 +1,19 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Box } from "@chakra-ui/react";
 
-const InfoBox = ({ info}) => {
+const InfoBox = ({ info }) => {
   const [edit, setEdit] = useState(false);
   const [expand, setExpand] = useState(false);
+  const [localValue, setLocalValue] = useState(info.value); // <-- local state
   const contentRef = useRef(null);
 
   useEffect(() => {
     if (edit && contentRef.current) {
       const element = contentRef.current;
-      element.focus(); // Focus on the contentEditable element
-      
+      element.focus();
+
       const range = document.createRange();
       const selection = window.getSelection();
-
-      // Move the caret to the end of the content
       range.selectNodeContents(element);
       range.collapse(false);
       selection.removeAllRanges();
@@ -22,9 +21,29 @@ const InfoBox = ({ info}) => {
     }
   }, [edit]);
 
+  useEffect(() => {
+    // Keep local value in sync with prop changes
+    setLocalValue(info.value);
+  }, [info.value]);
+
   const handleInput = (e) => {
     const newValue = e.target.textContent;
-    info?.handleFunction(newValue);
+    setLocalValue(newValue); // update locally
+    info?.handleFunction(newValue); // send to parent
+  };
+
+  const handleToggleEdit = () => {
+    if (edit) {
+      // if cancelling, reset local value
+      setLocalValue(info.value);
+    } else {
+      // if adding, clear value
+      if (!info.color) {
+        setLocalValue("");
+        info?.handleFunction("");
+      }
+    }
+    setEdit(!edit);
   };
 
   return (
@@ -39,60 +58,48 @@ const InfoBox = ({ info}) => {
     >
       <h1 style={{ fontSize: "17px", width: "25%" }}>{info?.title}</h1>
       <Box w={"64%"} display={"flex"} alignItems={"center"} justifyContent={"start"}>
-      <h1
-  ref={contentRef}
-  style={{
-    fontSize: "18px",
-    color: info?.color ? "black" : "gray",
-    outline: "none",
-  }}
-  contentEditable={edit}
-  suppressContentEditableWarning={true}
-  onInput={handleInput}
->
-  {!edit ? (
-    info.title === "Description" ? (
-      info.value.length > 80 ? (
-        expand ? info.value : info.value.slice(0, 80) + "..."
-      ) : (
-        info.value
-      )
-    ) : (
-      info.value
-    )
-  ) : (
-    info.value
-     // Show the value while editing
-  )}
-  {info.title === "Description" && info.value.length > 80 && !edit && (
-  <span
-    style={{ color: "blue", cursor: "pointer",marginLeft:"5px" }}
-    onClick={() => setExpand(!expand)}
-  >
-     read {expand ? "less" : "more"}
-  </span>
-)}
-</h1>
-
-
-
+        <h1
+          ref={contentRef}
+          style={{
+            fontSize: "18px",
+            color: info?.color ? "black" : "gray",
+            outline: "none",
+          }}
+          contentEditable={edit}
+          suppressContentEditableWarning={true}
+          onInput={handleInput}
+        >
+          {!edit ? (
+            info.title === "Description" ? (
+              info.value.length > 80 ? (
+                expand ? info.value : info.value.slice(0, 80) + "..."
+              ) : (
+                info.value
+              )
+            ) : (
+              info.value
+            )
+          ) : (
+            localValue
+          )}
+          {info.title === "Description" && info.value.length > 80 && !edit && (
+            <span
+              style={{ color: "blue", cursor: "pointer", marginLeft: "5px" }}
+              onClick={() => setExpand(!expand)}
+            >
+              read {expand ? "less" : "more"}
+            </span>
+          )}
+        </h1>
       </Box>
       {info.title !== "Email" && (
-        <button
-          style={{ color: "blue" }}
-          onClick={() => {
-            if (edit) {
-              info?.handleFunction("");
-            }
-            
-            setEdit(!edit);
-          }}
-        >
+        <button style={{ color: "blue" }} onClick={handleToggleEdit}>
           {!edit ? (info?.color ? "Edit" : "Add") : "Cancel"}
         </button>
       )}
     </Box>
   );
 };
+
 
 export default InfoBox;
